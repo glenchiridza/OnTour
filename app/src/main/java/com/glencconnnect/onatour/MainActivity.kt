dd5d3571ba3f8db2a3e2e1d45e691f692e7d6ee2
glenchiridza <glenchiridz@gmail.com>
2021-12-07 01:58:10 +0200

    our tour app ui

---
+++ app/src/main/java/com/glencconnnect/onatour/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.glencconnnect.onatour
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
