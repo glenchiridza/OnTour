e25a43221a8464ad1e296bcba6bfe9a56facaa5c
glenchiridza <glenchiridz@gmail.com>
2021-12-07 02:35:57 +0200

    navbar controllers

--- app/src/main/java/com/glencconnnect/onatour/MainActivity.kt
+++ app/src/main/java/com/glencconnnect/onatour/MainActivity.kt
@@ -2,16 +2,22 @@ package com.glencconnnect.onatour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.glencconnnect.onatour.R
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbar        : MaterialToolbar
     private lateinit var navController  : NavController
+    private lateinit var navigationView : NavigationView
+    private lateinit var draweLayout    : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +25,21 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Views
         toolbar         = findViewById(R.id.activity_main_toolbar)
+        navigationView         = findViewById(R.id.nav_view)
+        draweLayout         = findViewById(R.id.drawer_layout)
 
         // Get NavHostFragment and NavController
         val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
         navController   = navHostFrag.navController
+
+        //app bar cconfig connet drawelayout wit nav component
+        val appBarConfiguration = AppBarConfiguration(navController.graph, draweLayout)
+
+        // connect toolbar with navigation controller
+        toolbar.setupWithNavController(navController,appBarConfiguration)
+
+        //connect navigation view with nav controller
+        navigationView.setupWithNavController(navController)
+
     }
 }
